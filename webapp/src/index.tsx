ccdbef17185aaea14822e86a3c6116e31c83ecea
Joram Wilander <jwawilander@gmail.com>
2021-05-06 17:31:23 -0400

    Feedback fixes and typescript fixes

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -10,39 +10,39 @@ import {PluginRegistry} from './types/mattermost-webapp';
 export default class Plugin {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
     public async initialize(registry: PluginRegistry, store: Store<GlobalState, Action<Record<string, unknown>>>) {
-        window.store = store;
+        window.webappDebugStore = store;
         window.logStoreStatistics = () => logStoreStatistics(store);
     }
 }
 
-function logStoreStatistics(store) {
+function logStoreStatistics(store: any) {
     const state = store.getState();
 
     const currentChannelId = state.entities.channels.currentChannelId;
     const currentTeamId = state.entities.teams.currentTeamId;
 
     const statistics = {
-        'Users': Object.keys(state.entities.users.profiles).length,
-        'Teams': Object.keys(state.entities.teams.teams).length,
+        Users: Object.keys(state.entities.users.profiles).length,
+        Teams: Object.keys(state.entities.teams.teams).length,
         'Channels (all teams)': Object.keys(state.entities.channels.channels).length,
-        'Channels (current team)': Object.values(state.entities.channels.channels).filter((channel) => channel.team_id === currentTeamId).length,
-        'Channels (DMs/GMs)': Object.values(state.entities.channels.channels).filter((channel) => channel.team_id === '').length,
+        'Channels (current team)': Object.values(state.entities.channels.channels).filter((channel: any) => channel.team_id === currentTeamId).length,
+        'Channels (DMs/GMs)': Object.values(state.entities.channels.channels).filter((channel: any) => channel.team_id === '').length,
         'Sidebar categories (all teams)': Object.keys(state.entities.channelCategories.byId).length,
         'Sidebar categories (current team)': state.entities.channelCategories.orderByTeam[currentTeamId].length,
-        'Posts': Object.keys(state.entities.posts.posts).length,
-        'Posts (current channel)': Object.values(state.entities.posts.posts).filter((post) => post.channel_id === currentChannelId).length,
-        'Visible DMs (from preferences)': Object.values(state.entities.preferences.myPreferences).filter((preference) => preference.category === 'direct_channel_show' && preference.value === 'true').length,
-        'Visible GMs (from preferences)': Object.values(state.entities.preferences.myPreferences).filter((preference) => preference.category === 'group_channel_show' && preference.value === 'true').length,
+        Posts: Object.keys(state.entities.posts.posts).length,
+        'Posts (current channel)': Object.values(state.entities.posts.posts).filter((post: any) => post.channel_id === currentChannelId).length,
+        'Visible DMs (from preferences)': Object.values(state.entities.preferences.myPreferences).filter((preference: any) => preference.category === 'direct_channel_show' && preference.value === 'true').length,
+        'Visible GMs (from preferences)': Object.values(state.entities.preferences.myPreferences).filter((preference: any) => preference.category === 'group_channel_show' && preference.value === 'true').length,
     };
 
+    // eslint-disable-next-line no-console
     console.log('Objects loaded:', statistics);
 }
-
 
 declare global {
     interface Window {
         registerPlugin(id: string, plugin: Plugin): void
-        store: any;
+        webappDebugStore: any;
         logStoreStatistics: any;
     }
 }
