7b7a2bfee150ec1fd90ef3a44a33e1cae55e4f77
Joram Wilander <jwawilander@gmail.com>
2021-05-06 17:06:39 -0400

    Initial plugin setup

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -10,7 +10,7 @@ import {PluginRegistry} from './types/mattermost-webapp';
 export default class Plugin {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
     public async initialize(registry: PluginRegistry, store: Store<GlobalState, Action<Record<string, unknown>>>) {
-        // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
+        window.store = store;
     }
 }
 
